f3d37d2f54f5e717a02fce8831cab609bf8b34c4
KSHITIJ PAWAR <31236146+khp3040@users.noreply.github.com>
2021-05-18 20:04:31 +0530

    updated

--- src/main/java/com/myapp/spring/web/api/ProductAPI.java
+++ src/main/java/com/myapp/spring/web/api/ProductAPI.java
@@ -1,6 +1,5 @@
 package com.myapp.spring.web.api;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
--- src/test/java/com/myapp/spring/integration/ProductIntegrationTest.java
+++ src/test/java/com/myapp/spring/integration/ProductIntegrationTest.java
@@ -1,26 +1,19 @@
 package com.myapp.spring.integration;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
